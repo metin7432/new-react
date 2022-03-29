0a8780d384c5b9769a57789b034be308051f6b5d
Metin altay <39528623+metin7432@users.noreply.github.com>
2022-03-29 19:14:02 +0300

    Sarmalama

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Expenses from './components/expenses/Expenses';
+
 
 function App() {
+  const expenses = [
+  { id: 'e1', title :'Car Insurnce', amount : 265.56, date : new Date(2022, 3, 5) },
+  { id: 'e2', title :'House Insurnce', amount : 234.78, date : new Date(2022, 4, 8)},
+  { id: 'e3', title :' Electric bills', amount : 345.43, date : new Date(2022, 5, 23)},
+  { id: 'e3', title :'Water bills', amount : 123.45, date : new Date(2022, 8, 23)}
+   
+  
+
+]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+     <Expenses items={expenses} />
+     
     </div>
   );
 }
